106bcfd5cc24cbbd47fb8df5cab5db13671657b3
Bruno <b.b.da.costa@gmail.com>
2021-05-29 19:05:49 -0300

    :sparkles: Adição de funcionalidade de loop para  o elemento de UI progress circle

--- UI/ProgressElements/ProgressCircle/ProgressCircle.cs
+++ UI/ProgressElements/ProgressCircle/ProgressCircle.cs
@@ -3,27 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Assets.UnityFoundation.UI.ProgressElements.ProgressCircle {
-    public class ProgressCircle : MonoBehaviour {
+namespace Assets.UnityFoundation.UI.ProgressElements.ProgressCircle
+{
+    public class ProgressCircle : MonoBehaviour
+    {
+        [SerializeField] private float timerMax;
+        [SerializeField] private bool loop;
+        [SerializeField] private bool destroyWhenFinished;
 
         private Image mask;
         private float timer;
-        private float timerMax;
 
-        void Start() {
+        void Start()
+        {
             mask = transform.Find("mask").GetComponent<Image>();
         }
 
-        public void Setup(float timerMax) {
+        public void Setup(float timerMax, bool destroyWhenFinished = false)
+        {
             this.timerMax = timerMax;
+            this.destroyWhenFinished = destroyWhenFinished;
         }
 
-        private void Update() {
+        private void Update()
+        {
             timer += Time.deltaTime;
             mask.fillAmount = timer / timerMax;
 
-            if(timer >= timerMax) {
-                Destroy(gameObject);
+            if(timer >= timerMax)
+            {
+                if(loop)
+                {
+                    timer = 0f;
+                    return;
+                }
+
+                if(destroyWhenFinished) Destroy(gameObject);
+                else gameObject.SetActive(false);
             }
         }
     }
