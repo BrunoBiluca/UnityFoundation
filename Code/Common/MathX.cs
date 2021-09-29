335230a4055a0789911f3deacf48fd2d6f3b2389
Bruno <b.b.da.costa@gmail.com>
2021-09-29 18:05:19 -0300

    :sparkles: Adição de função para comparar floats com certa diferença

--- Code/Common/MathX.cs
+++ Code/Common/MathX.cs
@@ -7,6 +7,20 @@ namespace Assets.UnityFoundation.Code
     public static class MathX
     {
         public static float Remainder(float value) => Mathf.Floor(value) - value;
+
+        public static bool NearlyEqual(float a, float b, float epsilon)
+        {
+            float absA = Mathf.Abs(a);
+            float absB = Mathf.Abs(b);
+            float diff = Mathf.Abs(a - b);
+
+            if(a == b)
+                return true;
+            else if(a == 0 || b == 0 || diff < float.Epsilon)
+                return diff < epsilon;
+            else
+                return diff / (absA + absB) < epsilon;
+        }
     }
 
     public static class FloatMathExtensions
