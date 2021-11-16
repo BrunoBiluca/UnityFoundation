5547b54620b5b4532212114de4c8b6c24fa21be4
Bruno <b.b.da.costa@gmail.com>
2021-11-16 09:37:56 -0300

    :sparkles: Add constructor to Int2

--- Code/Common/Int2.cs
+++ Code/Common/Int2.cs
@@ -1,14 +1,25 @@
-﻿namespace Assets.UnityFoundation.Code.Common {
-    public class Int2 {
+﻿using UnityEngine;
+
+namespace Assets.UnityFoundation.Code.Common
+{
+    public class Int2
+    {
         private int x;
         private int y;
 
         public int X => x;
         public int Y => y;
 
-        public Int2(int x, int y) {
+        public Int2(int x, int y)
+        {
             this.x = x;
             this.y = y;
+        }
+
+        public Int2(Vector2 vector)
+        {
+            this.x = (int)vector.x;
+            this.y = (int)vector.y;
         }
     }
 }
