a0039cc33adfd1156bd2a53bc5d8a627826e4a44
Bruno <b.b.da.costa@gmail.com>
2022-01-24 08:53:12 -0300

    :sparkles: Add animation event proxy helper class

---
+++ Systems/Character3D/Scripts/Helpers/BaseCharacter3DAnimationEventProxy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Assets.UnityFoundation.Systems.Character3D.Scripts.Helpers
+{
+    /// <summary>
+    /// Helper MonoBehaviour class. Act as a proxy when the Animator component 
+    /// is in a child object of the Character 3D component. 
+    /// Pass through the animation event triggered by the animation of the child object.
+    /// </summary>
+    public class BaseCharacter3DAnimationEventProxy : MonoBehaviour
+    {
+        [SerializeField] private BaseCharacter3D baseCharacter;
+
+        private void Start()
+        {
+            if(baseCharacter == null)
+                baseCharacter = GetComponentInParent<BaseCharacter3D>();
+        }
+
+        public void TriggerAnimationEvent(string name)
+        {
+            baseCharacter.TriggerAnimationEvent(name);
+        }
+    }
+}
