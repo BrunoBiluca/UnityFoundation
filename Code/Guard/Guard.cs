4835db0a887e6b0ef63f3ffa5fea0e47129b426c
Bruno <b.b.da.costa@gmail.com>
2022-05-19 00:28:45 -0300

    :sparkles: Guard helper

---
+++ Code/Guard/Guard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnityFoundation.Code.Guard
+{
+    public static class Guard
+    {
+        public static T Required<T>(T param, string field)
+        {
+            return param != null
+                ? param
+                : throw new ArgumentException($"{field} is required");
+        }
+    }
+}
