6b3d2d8da4177cb953c4ad44cf6c34da6f9a4b2c
Bruno <b.b.da.costa@gmail.com>
2022-10-26 11:31:14 -0300

    ✨ Input view

--- Systems/Camera/CameraMovementXZ/CameraMovementXZController.cs
+++ Systems/Camera/CameraMovementXZ/CameraMovementXZController.cs
@@ -6,19 +6,45 @@ namespace UnityFoundation.CameraMovementXZ
     public class CameraMovementXZController : MonoBehaviour
     {
         [Header("Movement attributes")]
-        [SerializeField] private float cameraSpeed = 20f;
-        [SerializeField] private Rangef moveLimitsX;
-        [SerializeField] private Rangef moveLimitsY;
-        [SerializeField] private Rangef moveLimitsZ;
+
+        [SerializeField]
+        private float cameraSpeed = 20f;
+
+        [SerializeField]
+        [Tooltip("Limits in the X axis")]
+        private Rangef moveLimitsX;
+
+        [SerializeField]
+        [Tooltip("Limits in the Y axis")]
+        private Rangef moveLimitsY;
+
+        [SerializeField]
+        [Tooltip("Limits in the Z axis")]
+        private Rangef moveLimitsZ;
 
         [Header("Edge Movement")]
-        [SerializeField] private bool enableEdgeMovement = false;
-        [SerializeField] private float edgeOffset = 10f;
+
+        [SerializeField] 
+        [Tooltip("Enable movement when mouse cursor is on the edge of the screen")]
+        private bool enableEdgeMovement = false;
+
+        [SerializeField]
+        [Tooltip("Edge size for move camera")]
+        private float edgeOffset = 10f;
 
         [Header("Zoom attributes")]
-        [SerializeField] private bool enableZoom = false;
-        [SerializeField] private float zoomAmount = 2f;
-        [SerializeField] private float zoomSpeed = 5f;
+
+        [SerializeField]
+        [Tooltip("Enable movement in the Y axis")]
+        private bool enableZoom = false;
+
+        [SerializeField]
+        [Tooltip("Zoom's sensibility")]
+        private float zoomAmount = 2f;
+
+        [SerializeField] 
+        [Tooltip("Zoom's speed")]
+        private float zoomSpeed = 5f;
 
         private CameraMovementXZ cameraMovement;
 
---
+++ Systems/Camera/CameraMovementXZ/readme.md
@@ -0,0 +1,12 @@
+# Using
+
+```
+- Find the main camera 
+- Add the CameraController component
+```
+
+![](Doc/component.png)
+
+# Repository
+
+[CameraMovementXZ repository](https://github.com/BrunoBiluca/UnityFoundation/tree/main/Systems/Camera/CameraMovementXZ)
