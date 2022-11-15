62a745f44a7b34205a1416fa1b4d0228529266d8
Bruno <b.b.da.costa@gmail.com>
2022-11-15 19:59:20 -0300

    ✨ Add functions for ragdoll

---
+++ Systems/PhysicsSystems/Physics3D/RagdollHandler.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityFoundation.Code.UnityAdapter;
+using static UnityEngine.GraphicsBuffer;
+
+namespace UnityFoundation.Physics3D
+{
+    public class RagdollHandler : MonoBehaviour
+    {
+        [SerializeField] private Transform ragdollRoot;
+
+        public ITransform Root { get; private set; }
+
+        public void Awake()
+        {
+            Root = new TransformDecorator(ragdollRoot);
+        }
+
+        public void Setup(ITransform originalRoot)
+        {
+            MatchAllTransforms(Root, originalRoot);
+            ApplyForce(Root, transform.position, 300f, 10f);
+        }
+
+        private void MatchAllTransforms(ITransform clone, ITransform target)
+        {
+            foreach(ITransform child in target.GetChildren())
+            {
+                var originalChild = clone.Find(child.Name);
+
+                if(originalChild == null)
+                    continue;
+
+                child.Position = originalChild.Position;
+                child.Rotation = originalChild.Rotation;
+
+                MatchAllTransforms(child, originalChild);
+            }
+        }
+
+        private void ApplyForce(ITransform target, Vector3 position, float force, float radius)
+        {
+            foreach(ITransform child in target.GetChildren())
+            {
+                if(child.TryGetComponent(out Rigidbody rb))
+                    rb.AddExplosionForce(force, position, radius);
+
+                ApplyForce(child, position, force, radius);
+            }
+        }
+    }
+}
