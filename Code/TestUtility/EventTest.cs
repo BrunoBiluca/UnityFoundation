845d38ccb98856abf95d7a58f8aa8faf9dd97bfb
Bruno <b.b.da.costa@gmail.com>
2022-11-19 16:40:48 -0300

    ✨ Add Test for actions with parameters

--- Code/TestUtility/EventTest.cs
+++ Code/TestUtility/EventTest.cs
@@ -16,22 +16,48 @@ namespace UnityFoundation.TestUtility
         {
             WasTriggered = false;
             TriggerCount = 0;
-            SubscribeToEvent(sender, eventName);
+            SubscribeToEvent.Subscribe(sender, eventName, this, nameof(HandleTrigger));
         }
 
-        private void SubscribeToEvent(object sender, string eventName)
-        {
-            var eventInfo = sender.GetType().GetEvent(eventName);
-            var handler = Delegate.CreateDelegate(
-                eventInfo.EventHandlerType, this, nameof(HandleTrigger)
-            );
-            eventInfo.AddEventHandler(sender, handler);
-        }
+        protected virtual string GetCallbackName() => null;
 
-        private void HandleTrigger()
+        protected virtual void HandleTrigger()
         {
             WasTriggered = true;
             TriggerCount++;
         }
     }
+
+    public class EventTest<T> : EventTest
+    {
+        public new static EventTest<T> Create(object sender, string eventName)
+        {
+            return new EventTest<T>(sender, eventName);
+        }
+
+        public EventTest(object sender, string eventName) : base(sender, eventName)
+        {
+            SubscribeToEvent.Subscribe(sender, eventName, this, nameof(HandleTrigger));
+        }
+
+        private void HandleTrigger(T obj)
+        {
+            base.HandleTrigger();
+        }
+    }
+
+    public static class SubscribeToEvent
+    {
+        public static void Subscribe(
+            object sender, string eventName, object target, string callback
+        )
+        {
+            var eventInfo = sender.GetType().GetEvent(eventName);
+            var handler = Delegate.CreateDelegate(
+                eventInfo.EventHandlerType, target, callback
+            );
+            eventInfo.AddEventHandler(sender, handler);
+        }
+    }
+
 }
