5928c7d7eedb95c02e6f4b7d42e9f85819b065c0
Bruno <b.b.da.costa@gmail.com>
2023-01-17 14:40:33 -0300

    🦋 Add mock builder scripts

---
+++ Code/_Tests/Components/RaycastSelectorTests.cs
@@ -0,0 +1,60 @@
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFoundation.Code;
+using UnityFoundation.Code.UnityAdapter;
+
+namespace UnityFoundation.Code.Tests
+{
+    public class RaycastSelectorTests
+    {
+        [Test]
+        public void Should_select_anything_when_no_object_is_returned_by_raycast()
+        {
+            var raycast = new RaycastHandlerMockBuilder().Build();
+            var selector = new RaycastSelector(raycast);
+
+            var selected = selector.Select(Vector3.zero);
+
+            Assert.That(selected.IsPresent, Is.False);
+            Assert.That(selector.CurrentUnit.IsPresent, Is.False);
+        }
+
+        [Test]
+        public void Given_object_was_selected_should_unselected()
+        {
+            var raycast = new RaycastHandlerMockBuilder()
+                .WithReturnedObject<ISelectable>()
+                .Build();
+
+            var selector = new RaycastSelector(raycast);
+
+            selector.Select(Vector3.zero);
+
+            Assert.That(selector.CurrentUnit.IsPresent, Is.True);
+
+            selector.Unselect();
+
+            Assert.That(selector.CurrentUnit.IsPresent, Is.False);
+        }
+
+        [Test]
+        public void Should_select_when_object_returned_by_raycast_is_selectable()
+        {
+            var raycast = new RaycastHandlerMockBuilder()
+                .WithReturnedObject<ISelectable>()
+                .Build();
+
+            var selector = new RaycastSelector(raycast);
+
+            var selected = selector.Select(Vector3.zero);
+
+            Assert.That(selected.IsPresent, Is.True);
+            Assert.That(selector.CurrentUnit.IsPresent, Is.True);
+
+        }
+    }
+}
---
+++ Code/_Tests/_Mocks/RaycastHandlerMockBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using UnityEngine;
+using UnityFoundation.Code.UnityAdapter;
+using UnityFoundation.TestUtility;
+
+namespace UnityFoundation.Code.Tests
+{
+    public class RaycastHandlerMockBuilder : MockBuilder<IRaycastHandler>
+    {
+        private readonly Mock<IRaycastHandler> raycastHandler;
+        public RaycastHandlerMockBuilder()
+        {
+            raycastHandler = new Mock<IRaycastHandler>();
+        }
+
+        public RaycastHandlerMockBuilder WithReturnedObject<T>() where T : class
+        {
+            var expectedObject = new Mock<T>();
+            raycastHandler
+                .Setup(h => h.GetObjectOf<T>(It.IsAny<Vector2>(), It.IsAny<LayerMask>()))
+                .Returns(expectedObject.Object);
+
+            return this;
+        }
+
+        protected override Mock<IRaycastHandler> OnBuild()
+        {
+            return raycastHandler;
+        }
+    }
+}
