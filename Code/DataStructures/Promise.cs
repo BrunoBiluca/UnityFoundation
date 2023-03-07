ede75abf213046df13fc28544a4f8242aa63d662
Bruno <b.b.da.costa@gmail.com>
2023-03-07 12:52:57 -0300

    ♻️ Improve module cohesion between code features and data structures

--- Code/DataStructures/Promise.cs
+++ Code/DataStructures/Promise.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityFoundation.Code;
-using static UnityEngine.GraphicsBuffer;
 
 namespace UnityFoundation.Code.Promise
 {
