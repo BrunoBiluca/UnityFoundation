a5913998c63f705906a282d8b08241f330ff1087
Bruno <b.b.da.costa@gmail.com>
2023-06-13 15:16:01 -0300

    ✨ Improve Settings Panel setup to enable better configuration and open button

--- GameplaySystems/SettingsSystem/Assets/Prefabs/SimpleSettingsPanel/SettingsPanelController.cs
+++ GameplaySystems/SettingsSystem/Assets/Prefabs/SimpleSettingsPanel/SettingsPanelController.cs
@@ -1,51 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityFoundation.Code.Extensions;
 
 namespace UnityFoundation.SettingsSystem
 {
     public class SettingsPanelController : MonoBehaviour
     {
         [SerializeField] private bool panelStartClosed = true;
+        [SerializeField] private Button settingsBtn;
+        [SerializeField] private bool hideOnOpen = false;
+        [SerializeField] private float animationSpeed = 1f;
 
         private GameObject settingsPanel;
         private Animator settingsPanelAnimator;
-        private Button settingsBtn;
+
+        private bool isOpened;
 
         private void Awake()
         {
             settingsPanel = transform.Find("settings_panel").gameObject;
             settingsPanelAnimator = settingsPanel.GetComponent<Animator>();
+            settingsPanelAnimator.speed = animationSpeed;
+
             settingsPanel.transform.Find("back_btn")
                 .GetComponent<Button>()
                 .onClick
                 .AddListener(() => CloseSettingsPanel());
 
-            settingsBtn = transform.Find("settings_btn").GetComponent<Button>();
+            if(settingsBtn == null)
+                settingsBtn = transform.Find("settings_btn").GetComponent<Button>();
             settingsBtn.onClick.AddListener(() => OpenSettingsPanel());
 
+            isOpened = panelStartClosed;
             if(panelStartClosed)
                 settingsPanel.SetActive(false);
         }
 
         public void OpenSettingsPanel()
         {
+            StopCoroutine(nameof(CloseSettings));
             settingsPanel.SetActive(true);
             settingsPanelAnimator.Play("slide_in");
-            settingsBtn.gameObject.SetActive(false);
+
+            if(hideOnOpen)
+                settingsBtn.gameObject.SetActive(false);
         }
 
         public void CloseSettingsPanel()
         {
-            StartCoroutine(CloseSettings());
+            StartCoroutine(nameof(CloseSettings));
         }
 
         IEnumerator CloseSettings()
         {
             settingsPanelAnimator.Play("slide_out");
             yield return new WaitForSeconds(1f);
+
             settingsPanel.SetActive(false);
-            settingsBtn.gameObject.SetActive(true);
+
+            if(hideOnOpen)
+                settingsBtn.gameObject.SetActive(true);
         }
     }
 }
